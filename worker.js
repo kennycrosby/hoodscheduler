bef7aa14dc052f9f39b42a29c33cfdb9dd65cdbf
Kenny <ken.crosby65@gmail.com>
2015-11-12 18:44:59 -0800

    pushing to github

--- worker.js
+++ worker.js
@@ -5,6 +5,7 @@ var ig = require('instagram-node').instagram();
 var Firebase = require('firebase');
 
 var dataRef = new Firebase('https://hoodapp.firebaseio.com/news');
+var todaysDateRef;
 
 // Client IDs, Tokens, Secrets
 var twConfig = {
@@ -35,42 +36,55 @@ var igParams = {
   count : 25
 };
 
-var d = Date.now();
-var todaysDate = new Date(d).setHours(0,0,0,0);
-var fullDate = new Date().getTime();
+// var d = Date.now();
+// var todaysDate = new Date(d).setHours(0,0,0,0);
 
 // var todaysDate = 1446274800000; // Oct 31
+// var todaysDate = 1446624000000; // Nov 4
+// var todaysDate = 1446710400000; // Nov 5
+// var todaysDate = 1446883200000; // Nov 6
 // var todaysDate = 1446883200000; // Nov 7
 // var todaysDate = 1446969600000; // Nov 8
 // var todaysDate = 1447056000000; // Nov 9
 // var todaysDate = 1447142400000; // Nov 10
 // var todaysDate = 1447228800000; // Nov 11
 
+// console.log('todays date', todaysDate);
 
-console.log('todays date with hours', fullDate);
-console.log('todays date', todaysDate);
+// // see if todays date exists
+// dataRef.once('value', function(snapshot) {
+//   var dateSnapshot = snapshot.child(todaysDate).exists();
+//   if (dateSnapshot) {
+//     console.log('RECORD EXISTS');
+//     todaysDateRef = dataRef.child(todaysDate);
+//     // clear it out
+//     todaysDateRef.remove();
+//     todaysDateRef = dataRef.child(todaysDate);
+//     getData();
+//   } else {
+//     // its not there so create it
+//     todaysDateRef = dataRef.child(todaysDate);
+//     getData();
+//   }
+// });
 
-var todaysDateRef;
+function setDataDayObject() {
 
-// see if todays date exists
-dataRef.once('value', function(snapshot) {
-  var dateSnapshot = snapshot.child(todaysDate).exists();
-  if (dateSnapshot) {
-    console.log('RECORD EXISTS');
+}
+
+fillMonthWithData('Sept');
+
+function fillMonthWithData(month) {
+  // Database content filler
+  for(var i = 1; i < 31; i++) {
+    var todaysDate = new Date(month+' '+i+' 2015').getTime();
     todaysDateRef = dataRef.child(todaysDate);
-    // clear it out
-    todaysDateRef.remove();
-    todaysDateRef = dataRef.child(todaysDate);
-    getData();
-  } else {
-    // its not there so create it
-    // dataRef.update(todaysDate);
-    todaysDateRef = dataRef.child(todaysDate);
-    getData();
+    getData(todaysDate);
   }
-});
+}
 
-function getData() {
+
+function getData(today) {
 
   // request data
   ig.user_media_recent('315641205', igParams, function(err, medias, pagination, remaining, limit) {
@@ -84,8 +98,9 @@ function getData() {
       var instaDay = new Date(parseInt(instagrams[i].created_time) * 1000).setHours(0,0,0,0);
       console.log('current instagrams date without time', instaDay);
 
-      if (new Date(todaysDate).toDateString() === new Date(instaDay).toDateString()) { // dates match
+      if (new Date(today).toDateString() === new Date(instaDay).toDateString()) { // dates match
         console.log('WE HAVE A MATCH FOR INSTAGRAM');
+        todaysDateRef = dataRef.child(today);
         todaysDateRef.push(instagrams[i]);
       };
     }
@@ -103,13 +118,13 @@ function getData() {
       var twitterDay = new Date( Date.parse(tweets[i].created_at) ).setHours(0,0,0,0);
       console.log('current twitter date without time', twitterDay);
 
-      if (new Date(todaysDate).toDateString() === new Date(twitterDay).toDateString()) { // dates match
+      if (new Date(today).toDateString() === new Date(twitterDay).toDateString()) { // dates match
         console.log('WE HAVE A MATCH FOR TWITTER');
 
         var tweet = {
 
         }
-
+        todaysDateRef = dataRef.child(today);
         todaysDateRef.push(tweets[i]);
       }
     }
